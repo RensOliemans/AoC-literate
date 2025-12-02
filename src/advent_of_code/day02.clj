64a439d988cd814b8b98b3e56e95fabdc119dbce
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-02 10:07:43 +0100

    Clean

--- src/advent_of_code/day02.clj
+++ src/advent_of_code/day02.clj
@@ -2,26 +2,22 @@
   (:require [advent-of-code.utils :as u]))
 
 (defn- to-ranges
+  "Converts todays puzzle input to a seq of float-pairs.
+  For example, \"11-22,95-115\" will be converted to ((11 22) (95
+  115))."
   [input]
   (->> input
        u/to-csvs
        u/parse-ranges))
 
-(defn part-1
-  ([input] (part-1 input #"^(\d+)\1$"))
-  ([input regexp]
-   (->> input
-        to-ranges
-        (map (fn [[start end]] (range start (inc end))))
-        flatten
-        (map #(re-find regexp (str %)))
-        (filter identity)
-        (map first)
-        (map parse-long)
-        (reduce +))))
+(defn solve
+  [input regexp]
+  (->> input
+       to-ranges
+       (mapcat (fn [[start end]]
+                 (filterv #(re-matches regexp (str %))
+                          (range start (inc end)))))
+       (reduce +)))
 
-
-(defn part-2
-  "Day 02 Part 2"
-  [input]
-  (part-1 input #"^(\d+)\1+$"))
+(defn part-1 [input] (solve input #"^(\d+)\1$"))
+(defn part-2 [input] (solve input #"^(\d+)\1+$"))
