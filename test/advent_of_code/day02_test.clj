9aea8c0bc32e145e42d88275f73cb977c085cb6c
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-02 09:51:25 +0100

    Tests d2

--- test/advent_of_code/day02_test.clj
+++ test/advent_of_code/day02_test.clj
@@ -3,8 +3,10 @@
             [advent-of-code.day02 :refer :all]))
 
 (deftest testinput
-  (def inp "")
+  (def inp "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124")
   (testing "Part One"
-    (is (= "Implement this part" (part-1 inp))))
+    (is (= 1227775554 (part-1 inp))))
   (testing "Part Two"
-    (is (= "Implement this part" (part-2 inp)))))
+    (is (= 4174379265 (part-2 inp)))))
