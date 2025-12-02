0acd9b997a1ffada9d3d9ec803701c5df5e43ece
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-02 10:01:21 +0100

    Use regex

--- src/advent_of_code/day02.clj
+++ src/advent_of_code/day02.clj
@@ -7,41 +7,21 @@
        u/to-csvs
        u/parse-ranges))
 
-(defn- invalid?
-  "Returns true if n is invalid (sequence of digits repeated twice)"
-  [n]
-  (let [ns (str n)
-        halfway (/ (count ns) 2)]
-    (and (even? (count ns))
-         (= (subs ns 0 halfway)
-            (subs ns halfway)))))
-
 (defn part-1
-  "Day 02 Part 1"
-  ([input] (part-1 input invalid?))
-  ([input invalid-fn?]
+  ([input] (part-1 input #"^(\d+)\1$"))
+  ([input regexp]
    (->> input
         to-ranges
         (map (fn [[start end]] (range start (inc end))))
         flatten
-        (filter invalid-fn?)
+        (map #(re-find regexp (str %)))
+        (filter identity)
+        (map first)
+        (map parse-long)
         (reduce +))))
-
-(defn- any-invalid?
-  [n]
-  (let [ns (str n)
-        c (count ns)
-        ;; given an input "abc", parts will be ("a" "ab" "abc")
-        parts (rest (reductions str "" ns))
-        part-invalid? (fn [part]
-                        (let [pc (count part)]
-                          (and (= 0 (mod c pc))
-                               (< pc c)
-                               (= ns (apply str (repeat (/ c pc) part))))))]
-    (some part-invalid? parts)))
 
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  (part-1 input any-invalid?))
+  (part-1 input #"^(\d+)\1+$"))
